e9fc60e8b6428e4e0b815c79f6a66421d50c1821
mariotun <oriamnut09@gmail.com>
2023-11-01 21:03:22 -0600

    consultas peliculas

--- routes/peliculas.js
+++ routes/peliculas.js
@@ -127,4 +127,162 @@ router.get('/peliculas', async (req, res) => {
   });
 
 
+  //////////////////////////////////////////////// CONSULTAS ////////////////////////////////////////////////
+
+  router.get("/pconsulta1", async (req, res) => {
+    try {
+      const result = await dynamoDB.scan({ TableName: tableName }).promise();
+      res.json(result.Items);
+    } catch (error) {
+      res.status(500).json({ error: "Error al obtener las películas" });
+    }
+  });
+
+  router.get("/pconsulta2/:genero", async (req, res) => {
+    try {
+      const result = await dynamoDB.scan({
+        TableName: tableName,
+        FilterExpression: "#genero = :genero",
+        ExpressionAttributeNames: { "#genero": "Genero" },
+        ExpressionAttributeValues: { ":genero": req.params.genero }
+      }).promise();
+      res.json(result.Items);
+     } catch (error) {
+       res.status(500).json({ error: "Error al obtener las películas por género" });
+     }
+  });
+
+  router.get("/pconsulta3/:clasificacion", async (req, res) => {
+    try {
+      const result = await dynamoDB.scan({
+        TableName: tableName,
+        FilterExpression: "#clasificacion >= :clasificacion",
+        ExpressionAttributeNames: { "#clasificacion": "Clasificacion" },
+        ExpressionAttributeValues: { ":clasificacion": req.params.clasificacion }
+      }).promise();
+      res.json(result.Items);
+    } catch (error) {
+      res.status(500).json({ error: "Error al obtener las películas por clasificación" });
+    }
+  });
+
+  router.get("/pconsulta4/:director", async (req, res) => {
+    try {
+      const result = await dynamoDB.scan({
+        TableName: tableName,
+        FilterExpression: "#director = :director",
+        ExpressionAttributeNames: { "#director": "Director" },
+        ExpressionAttributeValues: { ":director": req.params.director }
+      }).promise();
+      res.json(result.Items);
+    } catch (error) {
+      res.status(500).json({ error: "Error al obtener las películas por director" });
+    }
+  });
+
+  router.get("/pconsulta5/:precio", async (req, res) => {
+    try {
+      const result = await dynamoDB.scan({
+        TableName: tableName,
+        FilterExpression: "#precio < :precio",
+        ExpressionAttributeNames: { "#precio": "Precio" },
+        ExpressionAttributeValues: { ":precio": Number(req.params.precio) }
+      }).promise();
+      res.json(result.Items);
+    } catch (error) {
+      res.status(500).json({ error: "Error al obtener las películas por precio" });
+    }
+  });
+
+  router.get("/pconsulta6/:anno", async (req, res) => {
+    try {
+      const result = await dynamoDB.scan({
+        TableName: tableName,
+        FilterExpression: "begins_with(FechaDeEstreno, :anno)",
+        ExpressionAttributeValues: { ":anno": req.params.anno }
+      }).promise();
+      res.json(result.Items);
+    } catch (error) {
+      res.status(500).json({ error: "Error al obtener las películas por año" });
+    }
+  });
+
+  router.get("/pconsulta7", async (req, res) => {
+    try {
+      const result = await dynamoDB.scan({
+        TableName: tableName,
+        ProjectionExpression: "Director, Calificacion",
+        FilterExpression: "attribute_exists(Calificacion)",
+      }).promise();
+  
+      // Agrupar por director y calcular el promedio
+      const directors = result.Items.reduce((acc, item) => {
+        if (!acc[item.Director]) {
+          acc[item.Director] = { total: 0, count: 0 };
+        }
+        acc[item.Director].total += item.Calificacion;
+        acc[item.Director].count += 1;
+        return acc;
+      }, {});
+  
+      // Calcular el promedio y ordenar
+      const avgRatings = Object.keys(directors).map((director) => ({
+        director,
+        promedio: directors[director].total / directors[director].count,
+      })).sort((a, b) => b.promedio - a.promedio).slice(0, 10);
+  
+      res.json(avgRatings);
+    } catch (error) {
+      res.status(500).json({ error: "Error al obtener los directores mejor calificados" });
+    }
+  });
+
+  router.get("/pconsulta8/:palabraClave", async (req, res) => {
+    try {
+      const result = await dynamoDB.scan({
+        TableName: tableName,
+        FilterExpression: "contains(Titulo, :palabraClave) or contains(Descripcion, :palabraClave)",
+        ExpressionAttributeValues: { ":palabraClave": req.params.palabraClave }
+      }).promise();
+      res.json(result.Items);
+     } catch (error) {
+       res.status(500).json({ error: "Error al realizar la búsqueda de películas" });
+     }
+  });
+
+  router.get("/pconsulta9", async (req, res) => {
+    try {
+      const result = await dynamoDB.scan({
+        TableName: tableName,
+        ProjectionExpression: "Precio",
+        FilterExpression: "attribute_exists(Precio)",
+      }).promise();
+  
+      const totalPrices = result.Items.reduce((total, item) => total + item.Precio, 0);
+      const avgPrice = totalPrices / result.Items.length;
+  
+      res.json({ precioPromedio: avgPrice });
+    } catch (error) {
+      res.status(500).json({ error: "Error al calcular el precio promedio de las películas" });
+    }
+  });
+
+  router.get("/pconsulta10", async (req, res) => {
+    try {
+      const result = await dynamoDB.scan({
+        TableName: tableName,
+        ProjectionExpression: "Titulo, Calificacion",
+        FilterExpression: "attribute_exists(Calificacion)",
+      }).promise();
+  
+      // Ordenar por calificación de mayor a menor
+      const sortedByRating = result.Items.sort((a, b) => b.Calificacion - a.Calificacion);
+  
+      res.json(sortedByRating);
+    } catch (error) {
+      res.status(500).json({ error: "Error al obtener las películas por mejor calificación" });
+    }
+  });
+    
+
   module.exports = router;
